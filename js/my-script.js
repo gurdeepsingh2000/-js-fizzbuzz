3663a8f18d4f6315f843e07b7ccf5f185ad54704
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-01 17:30:46 +0200

    update

--- js/my-script.js
+++ js/my-script.js
@@ -8,12 +8,19 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 var output = document.getElementById('number');
 
 for(var x = 1; x <= 100 ; x++){
-} if(x % 3){
-    output.innerHTML = "Fizz";
-} else if(x % 5){
-    output.innerHTML += "Buzz";
+  if(x % 3 === 0 && x % 5 === 0){
+    output.innerHTML += "<br/> FizzBuzz" ;
+
+} else if(x % 3 === 0){
+    output.innerHTML += " <br/> Fizz";
+
+} else if(x % 5 === 0){
+    output.innerHTML += "<br/> Buzz";
+
 } else{
-    output.innerHTML += "FizzBuzz";
+    output.innerHTML += "<br/>" + i;
 }
+}
+
 
 
